439d7c0657b7e6f3c6b32e0b8c1994f503022395
Kyriacos-Stavrinides <51713787+Kyriacos-Stavrinides@users.noreply.github.com>
2022-11-04 12:46:39 +0200

    few changes

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,8 @@
 window.onload = (event) => {
     let deviceWidth = window.screen.width;
     if (deviceWidth < 700) {
-        elementRemover("card__image");
+        
     } else if (deviceWidth < 900) {
-        classRemover("card", "shadow");
         
     } else if (deviceWidth < 1440) {
         // console.log(`Device width is ${deviceWidth}`);
@@ -14,16 +13,3 @@ window.onload = (event) => {
 
 
 
-function classRemover(element, className) {
-    let object = Array.from(document.getElementsByClassName(element));
-    for (const item of object) {
-        item.classList.remove(className);
-    }
-}
-
-function elementRemover(toDelete) {
-    let object = Array.from(document.getElementsByClassName(toDelete));
-    for (const item of object) {
-        item.remove();
-    }
-}
